817d7bf6e4199cfc0771756efcc2a9b00b03356f
ataffe <taffealexander@gmail.com>
2025-10-12 16:54:31 -0700

    Switched to gumbel softmax based quantizer

--- vqgan/model/vqgan.py
+++ vqgan/model/vqgan.py
@@ -37,9 +37,16 @@ class VQModel(pl.LightningModule):
         self.encoder = Encoder(**ddconfig)
         self.decoder = Decoder(**ddconfig)
         self.loss = instantiate_from_config(lossconfig)
-        self.quantize = VectorQuantizer(n_embed, embed_dim, beta=0.25,
-                                        remap=remap,
-                                        sane_index_shape=sane_index_shape)
+        # self.quantize = VectorQuantizer(n_embed, embed_dim, beta=0.25,
+        #                                 remap=remap,
+        #                                 sane_index_shape=sane_index_shape)
+        z_channels = ddconfig['z_channels']
+        self.quantize = GumbelQuantize(
+            num_hiddens=z_channels,
+            embedding_dim=embed_dim,
+            n_embed=n_embed,
+            kl_weight=1e-8,
+            straight_through=False)
         self.quant_conv = torch.nn.Conv2d(ddconfig["z_channels"], embed_dim, 1)
         self.post_quant_conv = torch.nn.Conv2d(embed_dim, ddconfig["z_channels"], 1)
         if colorize_nlabels is not None:
